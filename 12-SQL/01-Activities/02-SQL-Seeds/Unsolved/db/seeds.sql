9bedddcc4e3c171e0cd8fb1d6985827e1d125a5b
salem z <salem.zerezghi2@gmail.com>
2022-05-31 21:07:37 -0400

    db activities 1st and 2nd

---
+++ 12-SQL/01-Activities/02-SQL-Seeds/Unsolved/db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO books (id, book_name)
+VALUES 
+(1, 'Twilight'),
+(2, 'Your Birth PLan'),
+(3, 'The Devil Wears Prada'),
+(4, 'A Time To Kill'),
+(5, 'The Fault In Our Stars');
+
+INSERT INTO categories (id, category_name)
+VALUES
+(1, 'Fiction'),
+(2, 'Non Fiction');
+
+INSERT INTO prices (id, price)
+VALUE (1, 10.99);
