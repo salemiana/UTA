8914665e35622b001dfba2ea809b6f76bf365dfd
salem z <salem.zerezghi2@gmail.com>
2022-06-15 19:35:07 -0600

    upload

---
+++ 13-ORM/01-Activities/05-One-to-Many/Unsolved/models/index.js
@@ -0,0 +1,24 @@
+const Reader = require('./Reader');
+const Book = require('./Book');
+const LibraryCard = require('./LibraryCard');
+
+Reader.hasOne(LibraryCard, {
+  foreignKey: 'reader_id',
+  onDelete: 'CASCADE'
+});
+
+LibraryCard.belongsTo(Reader, {
+  foreignKey: 'reader_id'
+});
+
+
+Reader.hasMany(LibraryCard, {
+  foreignKey: 'reader_id',
+  onDelete: 'CASCADE'
+});
+
+book.belongsTo(Reader, {
+  foreignKey: 'reader_id'
+});
+
+module.exports = { Reader, Book, LibraryCard };
