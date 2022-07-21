8097036d9d8993a1b0f674a379084935ae51c67c
salem z <salem.zerezghi2@gmail.com>
2022-07-20 23:20:01 -0600

    general commit

--- 19-PWA/01-Activities/05-Notetaker-PWA/Unsolved/public/service-worker.js
+++ 19-PWA/01-Activities/05-Notetaker-PWA/Unsolved/public/service-worker.js
@@ -44,19 +44,42 @@ const FILES_TO_CACHE = [
         })
     
     // Intercept fetch requests
-    // YOUR CODE HERE
-    
     self.addEventListener('fetch', function(evt) {
+      if (evt.request.url.includes('/api/')) {
         evt.respondWith(
-            fetch(evt.request).catch(function() {
-              return caches.match(evt.request).then(function(response) {
-                if (response) {
+          caches
+            .open(DATA_CACHE_NAME)
+            .then(cache => {
+              return fetch(evt.request)
+                .then(response => {
+                  // If the response was good, clone it and store it in the cache.
+                  if (response.status === 200) {
+                    cache.put(evt.request.url, response.clone());
+                  }
+    
                   return response;
-                } else if (evt.request.headers.get('accept').includes('text/html')) {
-                  // return the cached home page for all requests for html pages
-                  return caches.match('/');
-                }
-              });
+                })
+                .catch(err => {
+                  // Network request failed, try to get it from the cache.
+                  return cache.match(evt.request);
+                });
             })
-          );
+            .catch(err => console.log(err))
+        );
+    
+        return;
+      }
+    
+      evt.respondWith(
+        fetch(evt.request).catch(function() {
+          return caches.match(evt.request).then(function(response) {
+            if (response) {
+              return response;
+            } else if (evt.request.headers.get('accept').includes('text/html')) {
+              // return the cached home page for all requests for html pages
+              return caches.match('/');
+            }
+          });
+        })
+      );
     });
